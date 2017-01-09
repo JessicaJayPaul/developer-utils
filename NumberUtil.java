9b4020c400f44f39d55bacb943e6ff9ec51b92e0
wulitaotao <879309896@qq.com>
2017-01-09 14:40:27 +0800

    处理一些小细节

--- NumberUtil.java
+++ NumberUtil.java
@@ -12,18 +12,14 @@ public class NumberUtil {
      * 获取2位小数
      */
     public static double getDvalue(double num){
-    	return getDvalue(num, ACCURACY);
+        return getDvalue(num, ACCURACY);
     }
     
     /**
      * 获取指定精度小数
      */
-	public static double getDvalue(double num, int accuracy){
-		BigDecimal b = new BigDecimal(num);
+    public static double getDvalue(double num, int accuracy){
+        BigDecimal b = new BigDecimal(num);
         return b.setScale(accuracy, BigDecimal.ROUND_HALF_UP).doubleValue();
-	}
-	
-	public static void main(String[] args) {
-		System.out.print(getDvalue(1.4452, 1));
-	}
+    }
 }
