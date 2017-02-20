10542e73c96a4a1a1383bbda004bba86eea7e339
wulitaotao <879309896@qq.com>
2017-02-20 10:00:25 +0800

    完善http请求工具类，提供header设置，响应回调

--- HttpUtil.java
+++ HttpUtil.java
@@ -5,70 +5,179 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * HTTP工具类
+ * @author wulitao
+ * @date 2017年2月19日
+ * @subscription
+ */
 public class HttpUtil {
     
+    /**
+     * 默认格式化编码格式
+     */
     public static final String DEFAULT_ENCODE = "UTF-8";
     
+    /**
+     * 系统换行符
+     */
     public static final String LINE_SEPARATOR = "line.separator";
+    
+    /**
+     * http连接信息
+     */
+    private HttpURLConnection connection;
+    
+    /**
+     * 请求回调接口（用于获取响应头，提取保存的cookie信息）
+     */
+    private Callback callback;
+    
+    /**
+     * 请求参数
+     */
+    private String data;
+    
+    /**
+     * 私有构造方法
+     */
+    private HttpUtil(){}
+    
+    /**
+     * 实现类
+     * @return
+     */
+    public static HttpUtil getInstance(){
+        return new HttpUtil();
+    }
+    
+    /**
+     * 初始化访问地址url
+     * @param url
+     * @return
+     */
+    public HttpUtil connect(String url){
+        try {
+            connection = (HttpURLConnection) new URL(url).openConnection();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+    
+    /**
+     * 设置请求参数，注意格式化编码
+     * @param key
+     * @param value
+     * @return
+     */
+    public HttpUtil data(String key, String value){
+        String curData = encodeString(key) + "=" + encodeString(value);
+        if (data == null || data.equals("")) {
+            data = curData;
+        } else {
+            data += "&" + curData;
+        }
+        return this;
+    }
+    
+    /**
+     * 设置请求参数，类型为map
+     * @param map
+     * @return
+     */
+    public HttpUtil data(Map<String, String> map) {
+        data = getParamter(map);
+        return this;
+    }
+    
+    /**
+     * 设置请求头部
+     * @param key
+     * @param value
+     * @return
+     */
+    public HttpUtil header(String key, String value){
+        connection.addRequestProperty(key, value);
+        return this;
+    }
+    
+    /**
+     * 设置请求cookie
+     * @param key
+     * @param value
+     * @return
+     */
+    public HttpUtil cookie(String key, String value){
+        connection.addRequestProperty("Cookie", key + "=" + value);
+        return this;
+    }
 
     /**
-     * 发起get请求，获取返回文本信息
+     * 设置请求响应接口
+     * @param callback
+     * @return
      */
-    public static String doGet(String url){
-        return doGet(url, null);
-    }
-    
-    public static String doGet(String url, Map<String, String> map){
-        return doGet(url, map, null);
-    }
-    
-    public static String doGet(String url, Map<String, String> map, Map<String, String> propertyMap){
-        HttpURLConnection connection = null;
-        // 使用StringBuilder避免在while循环体中多次创建String对象而浪费资源
-        StringBuilder builder = new StringBuilder();
-        try {
-            if (map != null && !map.isEmpty()) {
-                url = url + "?" + getParamter(map);
-            }
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            setRequestProperty(connection, propertyMap);
-            readData(connection, builder);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally{
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        return builder.toString();
+    public HttpUtil callBack(Callback callback){
+        this.callback = callback;
+        return this;
     }
     
     /**
-     * 发起post请求，获取返回文本信息
+     * 发起get请求，返回响应数据（String）
+     * @return
      */
-    public static String doPost(String url, Map<String, String> map){
-        return doPost(url, map, null);
+    public String get(){
+        return getResponseStr("GET");
     }
     
-    public static String doPost(String url, Map<String, String> map, Map<String, String> propertyMap){
-        HttpURLConnection connection = null;
+    /**
+     * 发起post请求，返回相应数据（String）
+     * @return
+     */
+    public String post(){
+        return getResponseStr("POST");
+    }
+    
+    /**
+     * 发起http访问获取响应数据
+     * @param method
+     * @return
+     */
+    public String getResponseStr(String method){
+        // 采用builder，
         StringBuilder builder = new StringBuilder();
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            // post请求必须设置此项
+            connection.setRequestMethod(method);
             connection.setDoOutput(true);
-            setRequestProperty(connection, propertyMap);
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), DEFAULT_ENCODE));
-            writer.write(getParamter(map));
-            writer.flush();
-            writer.close();
-            readData(connection, builder);
+            BufferedWriter writer;
+            if (data != null && !data.equals("")) {
+                // 传参不为空才进行赋值
+                writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), DEFAULT_ENCODE));
+                writer.write(data);
+                writer.flush();
+                writer.close();
+            }
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), DEFAULT_ENCODE));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                    builder.append(System.getProperty(LINE_SEPARATOR));
+                }
+                if (callback != null) {
+                    callback.success(connection, builder.toString());
+                }
+                reader.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally{
@@ -77,48 +186,56 @@ public class HttpUtil {
             }
         }
         return builder.toString();
-    }
-    
-    /**
-     * 设置请求参数
-     */
-    public static void setRequestProperty(HttpURLConnection connection, Map<String, String> map){
-        if (map == null || map.isEmpty()) {
-            return;
-        }
-        for (Entry<String, String> entry : map.entrySet()) {
-            connection.setRequestProperty(entry.getKey(), entry.getValue());
-        }
     }
     
     /**
      * 将封装的map参数转换为字符串，以便写入输出流中
      */
-    public static String getParamter(Map<String, String> map) throws UnsupportedEncodingException{
-        if (map == null || map.isEmpty()) {
-            return "";
-        }
+    public static String getParamter(Map<String, String> map){
         StringBuilder builder = new StringBuilder();
         // encode下字符串，避免参数含有特殊字符
-        for (Entry<String, String> entry : map.entrySet()) {
-            builder.append(URLEncoder.encode(entry.getKey(), DEFAULT_ENCODE));
-            builder.append("=");
-            builder.append(URLEncoder.encode(entry.getValue(), DEFAULT_ENCODE));
-            builder.append("&");
+        try {
+            for (Entry<String, String> entry : map.entrySet()) {
+                builder.append(URLEncoder.encode(entry.getKey(), DEFAULT_ENCODE));
+                builder.append("=");
+                builder.append(URLEncoder.encode(entry.getValue(), DEFAULT_ENCODE));
+                builder.append("&");
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
         // 去掉最后一个&
-        return builder.substring(0, builder.length() - 1);
+        if (builder.length() > 0) {
+            return builder.substring(0, builder.length() - 1);
+        }
+        return builder.toString();
     }
     
-    public static void readData(HttpURLConnection connection, StringBuilder builder) throws UnsupportedEncodingException, IOException{
-        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), DEFAULT_ENCODE));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-                builder.append(System.getProperty(LINE_SEPARATOR));
-            }
-            reader.close();
+    
+    /**
+     * 格式化编码
+     * @param str
+     * @return
+     */
+    public static String encodeString(String str){
+        String result = "";
+        try {
+            result = URLEncoder.encode(str, DEFAULT_ENCODE);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
+        return result;
+    }
+
+    /**
+     * http请求接口回调接口
+     */
+    interface Callback{
+        
+        /**
+         * 主要用于多线程回调
+         * @param connection
+         */
+        void success(HttpURLConnection connection, String response);
     }
 }
